87732af5c2b3107c9d4e8f4e37ba2ce0d5e58a51
Jose Maria Perez Bordagaray <ppbordagaray@gmail.com>
2015-04-24 15:23:59 -0300

    Cambios en la Vista

--- Teoria Trabajo/src/teoInfo/model/metricas/Media.java
+++ Teoria Trabajo/src/teoInfo/model/metricas/Media.java
@@ -6,10 +6,18 @@ import java.util.List;
 import teoInfo.model.huffman.Simbolo;
 
 public class Media {
-
+	public static String formula;
+	public String getFormula() {
+		return formula;
+	}
+	public void setFormula(String formula) {
+		this.formula = formula;
+	}
 	public static double calcular(List<Simbolo> fs, HashMap<Simbolo,String> codificacion){
 		double longMedia = 0.0;
+		formula = new String();
 		for(int i=0; i<fs.size(); i++){
+			formula = formula +" + "+ fs.get(i).getProb() +" * "+((String)codificacion.get(fs.get(i))).length();
   			longMedia = longMedia + fs.get(i).getProb() * ((String)codificacion.get(fs.get(i))).length();
   		}
 		return longMedia;
